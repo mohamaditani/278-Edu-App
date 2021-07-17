16f7b24291a309170960b99867b632e87cbf1740
mohamaditani <>
2021-07-17 15:21:54 +0300

    Done with Course Selector js, set up English page, connected course selector to English and Math

--- Course Selector/courseSelector.js
+++ Course Selector/courseSelector.js
@@ -3,24 +3,34 @@ window.onload = function() {
 }
 
 function englishFunc() {
-  createButtons();
+  createButtons("English");
 }
 
-function createButtons() {
-  let buttonDiv = document.getElementById("buttonBox");
+function mathFunc(){
+  createButtons("math");
+}
 
+function createButtons(subject) {
+  let buttonDiv = document.getElementById("buttonBox");
+  buttonDiv.innerHTML = '';
 
   let easy = document.createElement("button");
-  easy.innerHTML = "EASY";
+  easy.innerHTML = "EASY " + subject;
   easy.setAttribute("class", "easy diffButton");
+  easy.id= "easy" + subject
+  easy.onclick = function(){window.location.href = "../Question"+subject+"/"+subject+"QPage.html?pageDif=easy"}
 
   let medium = document.createElement("button");
-  medium.innerHTML = "MEDIUM";
+  medium.innerHTML = "MEDIUM " + subject;
   medium.setAttribute("class", "medium diffButton");
+  medium.id= "easy" + subject
+  medium.onclick = function(){window.location.href = "../Question"+subject+"/"+subject+"QPage.html?pageDif=medium"}
 
   let hard = document.createElement("button");
-  hard.innerHTML = "HARD";
+  hard.innerHTML = "HARD "+ subject;
   hard.setAttribute("class", "hard diffButton");
+  hard.id= "easy" + subject
+  hard.onclick = function(){window.location.href = "../Question"+subject+"/"+subject+"QPage.html?pageDif=hard"}
 
 
 
