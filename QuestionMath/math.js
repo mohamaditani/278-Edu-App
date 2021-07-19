1cbe9862697c823bd74b64fe1c64b57b8ffe9c19
mohamaditani <>
2021-07-19 04:49:31 +0300

    audio, colors, difficulty in math section

--- QuestionMath/math.js
+++ QuestionMath/math.js
@@ -1,7 +1,13 @@
-var maxTime = 1;
+var maxTime = 0;
+var lockTime = 0;
 var questionCounter = 0;
 var currentScore = 0;
 var timers = null;
+var questionCount = 0;
+var difficultyMultiplyer = 1;
+var audioRight = new Audio('../Audio/Right.mp3');
+var audioWrong = new Audio('../Audio/Wrong.mp3');
+var skipTimer = false;
 
 window.onload = function() {
   setDif();
@@ -9,8 +15,27 @@ window.onload = function() {
 };
 
 function setDif() {
+  let url = new URL(window.location.href);
+  let difficulty = url.searchParams.get('pageDif');
+
+  if (difficulty === "easy") {
+    questionCount = 5;
+    maxTime = 10;
+    lockTime = 10;
+    difficultyMultiplyer = 10;
+  } else if (difficulty === "medium") {
+    questionCount = 10;
+    maxTime = 15;
+    lockTime = 15;
+    difficultyMultiplyer = 30;
+  } else {
+    questionCount = 15;
+    maxTime = 20;
+    lockTime = 20;
+    difficultyMultiplyer = 50;
+  }
   let totalScore = document.getElementById("total");
-  totalScore.innerText = "5"; //fix
+  totalScore.innerText = questionCount;
 
 }
 
@@ -23,8 +48,8 @@ function nextProblem() {
   let allOpps = ["+", "-", "*", "%", "/"];
   // let selectOpp = parseInt(Math.random() * 5);
 
-  let number1 = parseInt(Math.random() * 1) + 1;
-  let number2 = parseInt(Math.random() * 1) + 1;
+  let number1 = parseInt(Math.random() * difficultyMultiplyer) + 1;
+  let number2 = parseInt(Math.random() * difficultyMultiplyer) + 1;
   let opperator = allOpps[parseInt(Math.random() * 5)];
 
   num1.innerHTML = number1;
@@ -36,9 +61,14 @@ function nextProblem() {
   let htmlTime = document.getElementById("timeNum");
   htmlTime.innerText = maxTime;
 
-maxTime = 2;
-clearInterval(timers);
-timers = setInterval(function(){timer(answer)}, 1000);
+
+  maxTime = lockTime + 1;
+  clearInterval(timers);
+
+
+  timers = setInterval(function() {
+    timer(answer)
+  }, 1000);
 
 
 }
@@ -74,17 +104,29 @@ function timer(answer) {
   let guess = parseInt(document.getElementById("guess").value);
   let htmlTime = document.getElementById("timeNum");
   let scoreCounter = document.getElementById("correct");
+  let InfoBox = document.getElementById("iBox");
   htmlTime.innerText = maxTime;
 
-  if(maxTime === 0){
-    if(guess === answer){
+  if (maxTime === 0) {
+    if (guess === answer) {
       currentScore += 1;
       scoreCounter.innerHTML = currentScore;
+      InfoBox.classList.add("backgroundColorRight");
+      audioRight.play();
+      setTimeout(function() {
+        InfoBox.classList.remove("backgroundColorRight");
+      }, 400);
+    }else{
+      audioWrong.play();
+      InfoBox.classList.add("backgroundColorWrong");
+      setTimeout(function() {
+        InfoBox.classList.remove("backgroundColorWrong");
+      }, 400);
     }
     questionCounter += 1;
     document.getElementById("guess").value = "";
 
-    if(questionCounter === 5){
+    if (questionCounter === questionCount) {
       endFunction();
     }
 
@@ -92,6 +134,10 @@ function timer(answer) {
   }
 }
 
-function endFunction(){
+function endFunction() {
   alert(currentScore)
 }
+// 
+// function submitQuiz(){
+// skipTimer = true;
+// }
